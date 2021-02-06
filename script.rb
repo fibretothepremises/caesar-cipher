8480779e25621f535dc6d71bed881dd1f6535f9d
Fibre P <fttx5525@gmail.com>
2021-02-06 11:33:47 +1100

    Initial commit

---
+++ script.rb
@@ -0,0 +1,30 @@
+def caesar_cipher(string, shift_num)
+  lowercase = ("a".."z").to_a
+  uppercase = ("A".."Z").to_a
+  string_array = []
+  string.each_char do |c|
+    if lowercase.include?(c)
+      new_index = (lowercase.index(c) + shift_num) % 26
+      string_array.push(lowercase[new_index])
+    elsif uppercase.include?(c)
+      new_index = (uppercase.index(c) + shift_num) % 26
+      string_array.push(uppercase[new_index])
+    else
+      string_array.push(c)
+    end
+  end
+  new_string = string_array.join("")
+  p new_string
+end
+
+caesar_cipher("What a string!", 5)
+caesar_cipher("What a string!", 51)
+caesar_cipher("What a string!", 52)
+caesar_cipher("What a string!", -51)
+caesar_cipher("What a string!", -52)
+# > caesar_cipher("What a string!", 5)
+#   => "Bmfy f xywnsl!"6
+
+# You will need to remember how to convert a string into a number.
+# Don’t forget to wrap from z to a.
+# Don’t forget to keep the same case.
